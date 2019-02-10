04c6e37ad1bbc69c0c87dc6d273f168aaabe188e
zhengant <zhengant@gmail.com>
2019-02-10 16:54:04 -0500

    returning string as well as logging message in playback functions

--- functions/playback/playback_functions.js
+++ functions/playback/playback_functions.js
@@ -7,16 +7,24 @@
 
 exports.play = function() {
      console.log("Called play");
+
+     return "Called play";
 }
 
 exports.pause = function() {
      console.log("Called pause");
+
+     return "Called pause";
 }
 
 exports.next = function {
      console.log("Called next");
+
+     return "Called next";
 }
 
 exports.previous = function {
      console.log("Called previous");
+
+     return "Called previous";
 }
